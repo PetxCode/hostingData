9f8b32066135379ca38350eaa96a19229d1daf9f
PetxCode <71887594+PetxCode@users.noreply.github.com>
2021-10-19 19:23:14 +0100

    hackerthon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import HomeScreen from "./components/HomeScreen";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HeaderNav from "./components/HeaderNav";
+import MovieFile from "./components/MovieFile";
+import MovieDetail from "./components/MovieDetail";
+import APIData from "./components/APIData";
+import { Hackerthon } from "./Hackerthon/Hackerthon";
 const App = () => {
   return (
     <BrowserRouter>
-      <HeaderNav />
+      {/* <HeaderNav /> */}
       <Switch>
-        <Route path="/" exact component={HomeScreen} />
+        <Route path="/" exact component={Hackerthon} />
+        <Route path="/api" exact component={APIData} />
+        <Route path="/detail/:id" exact component={MovieDetail} />
       </Switch>
     </BrowserRouter>
   );
